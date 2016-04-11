976743ead9c6fc0aabc7a59dcb9b66767eb23048
jenia <gfjenia@gmail.com>
2016-04-11 20:16:34 +0300

    add gitignore

--- src/by/bsu/chat/action/HistoryAction.java
+++ src/by/bsu/chat/action/HistoryAction.java
@@ -1,4 +1,4 @@
-package by.bsu.chat.logic;
+package by.bsu.chat.action;
 
 import by.bsu.chat.entity.Message;
 import java.util.ArrayDeque;
@@ -6,19 +6,30 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class HistoryAction {
     public void printMessages(Collection<Message> messages) {
-        if (!messages.isEmpty()) {
-            messages.forEach(System.out::println);
-        } else {
+        if (messages.isEmpty()) {
             System.out.println("History is empty");
+            return;
+        }
+        for (Message message : messages) {
+            System.out.println(message);
+            System.out.println();
+        }
+    }
 
+    public boolean removeById(int id, Collection<Message> messages) {
+        Collection<Message> newMessages = new ArrayDeque<>();
+        for (Message message : messages) {
+            if (message.getId() != id) {
+                newMessages.add(message);
+            }
         }
 
-    }
-    public boolean removeMessage(int id, Collection<Message> messages) {
-        Iterator<Message> iterator = messages.iterator();
+        return true;
+        /*Iterator<Message> iterator = messages.iterator();
         Message message;
         while (iterator.hasNext()) {
             message = iterator.next();
@@ -26,7 +37,7 @@ public class HistoryAction {
                 return messages.remove(message);
             }
         }
-        return false;
+        return f;*/
     }
 
     public Collection<Message> authorSearch(String author, Collection<Message> messages) {
@@ -39,29 +50,20 @@ public class HistoryAction {
         return deque;
     }
 
-    public Collection<String> regexSearch(String regex, Collection<Message> messages) {
-        Collection<String> deque = new ArrayDeque<>();
+    public Collection<Message> regexSearch(String regex, Collection<Message> messages) {
+        Collection<Message> deque = new ArrayDeque<>();
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher;
         for (Message message : messages) {
             matcher = pattern.matcher(message.getMessage());
-            while (matcher.find()) {
-                deque.add(matcher.group());
+            if (matcher.find()) {
+                deque.add(message);
             }
         }
         return deque;
     }
 
-    /*public List<Message> lexemeSearch(String lexeme, List<Message> messages) {
-        List<Message> list = new ArrayList<>();
-        Pattern pattern = Pattern.compile(lexeme);
-        Matcher matcher;
-        for (Message message : messages) {
-            matcher = pattern.matcher(message.getMessage());
-            if (matcher.find()) {
-                list.add(message);
-            }
-        }
-        return list;
-    }*/
+    public Collection<Message> lexemeSearch(String lexeme, Collection<Message> messages) {
+        return regexSearch(lexeme, messages);
+    }
 }
